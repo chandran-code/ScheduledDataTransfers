70f0ca04c60804335f829edc1e8a5cf3127a6e62
chandran.code <chandran.code@gmail.com>
2024-03-26 21:47:53 +0530

    removed snowflake

--- generic_client2.py
+++ generic_client2.py
@@ -3,18 +3,7 @@ to the role of the lambda, or explicitly mention the access key/id in the code. 
 below. I think role is used in aws env and access key/id is iused while testing locally """
 
 # import pandas as pd
-import generic_client1 as gc1
-import generic_client2 as gc2
 
-def lambda_handler(event, context):
+def transform_data():
 
-    print("In the lambda ")
-    gc1.transform_data()
-    gc2.transform_data()
-
-    return {
-        'statusCode': 200,
-        'body': 'Messages processed successfully'
-    }
-
-lambda_handler(None, None)
+    print("In generic_client2")
