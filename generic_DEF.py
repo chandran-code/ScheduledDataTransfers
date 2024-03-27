ca58939d73a910af8981ea195f75cbc19dc898bc
chandran.code <chandran.code@gmail.com>
2024-03-27 22:21:46 +0530

    cleanup

--- generic_DEF.py
+++ generic_DEF.py
@@ -1,8 +1,3 @@
-""" About AWS authentication while connecting to s3 - There are two ways to do it. Either add s3 full aws_access_key_id
-to the role of the lambda, or explicitly mention the access key/id in the code. Both the options are
-below. I think role is used in aws env and access key/id is iused while testing locally """
-
-# import pandas as pd
 
 def transform_data():
 
